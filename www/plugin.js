8aee599d8466f8b455a7ce66e29a7dfeccb2b4ab
Alex Serga <snike1985@gmail.com>
2019-06-26 14:14:28 +0300

    test plugin

--- www/plugin.js
+++ www/plugin.js
@@ -1,8 +1,5 @@
-var argscheck = require('cordova/argscheck');
 var channel = require('cordova/channel');
-var utils = require('cordova/utils');
 var exec = require('cordova/exec');
-var cordova = require('cordova');
 
 channel.createSticky('onCordovaInfoReady');
 // Tell cordova channel to wait on the CordovaInfoReady event
@@ -15,7 +12,7 @@ channel.waitForInitialization('onCordovaInfoReady');
  */
 const PLUGIN_NAME = 'CordovaIntercomPlugin';
 
-const CordovaIntercomPlugin =  {
+const CordovaIntercomPlugin = {
     echo: function (phrase, callback) {
         exec(callback, null, PLUGIN_NAME, 'echo', [phrase]);
     },
