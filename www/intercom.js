e0cb733306a0da5e2792b7ee657ee77aae8076ce
Alex Serga <snike1985@gmail.com>
2019-07-01 09:56:11 +0300

    test echo

--- www/intercom.js
+++ www/intercom.js
@@ -5,6 +5,9 @@ var PLUGIN_NAME = 'Intercom';
 var Intercom = {
     echo: function(phrase, cb) {
         exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
+    },
+    getDate: function(cb) {
+        exec(cb, null, PLUGIN_NAME, 'getDate', []);
     }
 };
 
